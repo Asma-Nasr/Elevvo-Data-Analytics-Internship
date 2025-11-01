fcb7ea0d376b243c0f5d5688d9c755b45106e259
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-11-01 20:08:31 +0200

    Add files via upload

---
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/revenue_by_country.sql
@@ -0,0 +1,8 @@
+SELECT 
+    c.Country,
+    ROUND(SUM(il.Quantity * il.UnitPrice), 2) AS TotalRevenue
+FROM InvoiceLine il
+JOIN Invoice i ON il.InvoiceId = i.InvoiceId
+JOIN Customer c ON i.CustomerId = c.CustomerId
+GROUP BY c.Country
+ORDER BY TotalRevenue DESC;
