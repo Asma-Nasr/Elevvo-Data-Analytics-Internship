c9c8f35873433312a2a0339da59f9e630e4ae54b
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2025-11-06 20:56:00 +0200

    Update revenue_by_country.sql

--- Task 5 SQL-Based Analysis of Product Sales/SQL Queries/revenue_by_country.sql
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/revenue_by_country.sql
@@ -5,4 +5,4 @@ FROM InvoiceLine il
 JOIN Invoice i ON il.InvoiceId = i.InvoiceId
 JOIN Customer c ON i.CustomerId = c.CustomerId
 GROUP BY c.Country
-ORDER BY TotalRevenue DESC;
+ORDER BY TotalRevenue DESC; 
