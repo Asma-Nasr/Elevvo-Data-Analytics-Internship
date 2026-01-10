b8ab6ffabab3f046ad181d6b63eacd4dfb61b158
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-10 21:52:06 +0200

    Create top_spending_customers.sql

---
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/top_spending_customers.sql
@@ -0,0 +1,11 @@
+/*
+Who are the top 10 customers by total spending?
+*/
+SELECT c.CustomerId,
+		FirstName || ' ' || LastName AS 'Full Name',
+		ROUND(SUM(i.Total)) AS Total
+FROM Customer c
+JOIN Invoice i ON c.CustomerId = i.CustomerId
+GROUP BY i.CustomerId
+ORDER BY Total DESC
+LIMIT 10;
