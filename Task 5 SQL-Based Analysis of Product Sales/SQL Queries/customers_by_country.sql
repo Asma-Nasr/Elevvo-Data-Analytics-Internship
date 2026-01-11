6feaf9df0ba3b20e1b134815c677d90d8373d1ce
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-11 20:26:33 +0200

    Update customers_by_country.sql

--- Task 5 SQL-Based Analysis of Product Sales/SQL Queries/customers_by_country.sql
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/customers_by_country.sql
@@ -4,4 +4,4 @@ How many customers are there in each country?
 SELECT Country, COUNT(*) AS Customer_count
 FROM Customer
 GROUP BY Country
-ORDER BY Customer_count DESC;
+ORDER BY Customer_count DESC; 
