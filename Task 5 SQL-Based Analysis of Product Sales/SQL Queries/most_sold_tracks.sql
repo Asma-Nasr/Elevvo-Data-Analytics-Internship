c12ab31a54efa77bb1c8127e0b5cefb67f079ec8
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-12 18:57:24 +0200

    Create most_sold_tracks.sql
    Which 10 tracks have been sold the most (by quantity)?

---
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/most_sold_tracks.sql
@@ -0,0 +1,9 @@
+/*
+Which 10 tracks have been sold the most (by quantity)?
+*/
+SELECT t.Name, SUM(i.Quantity) AS Quantity
+FROM Track t
+JOIN InvoiceLine i ON t.TrackId = i.TrackId
+GROUP BY t.Name
+ORDER BY Quantity DESC
+LIMIT 10;
