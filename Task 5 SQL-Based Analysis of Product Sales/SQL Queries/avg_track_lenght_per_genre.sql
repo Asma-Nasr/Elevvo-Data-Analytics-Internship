65a8d993c0ccd344a63a7dcfc3cb2ef6843eedff
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-12 22:43:54 +0200

    Create avg_track_lenght_per_genre.sql
    What is the average track length (in minutes) for each genre?

---
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/avg_track_lenght_per_genre.sql
@@ -0,0 +1,8 @@
+/*
+What is the average track length (in minutes) for each genre?
+*/
+SELECT g.Name, ROUND(AVG(t.Milliseconds/60000),2) AS avg_lenght_minutes
+FROM Genre g 
+JOIN Track t ON g.GenreId = t.GenreId
+GROUP BY g.Name
+ORDER BY avg_lenght_minutes DESC;
