59b8c3b4bada80cf6157c875aeb65fcf0893a7d5
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-01-27 18:11:21 +0200

    Create albums_more_than_20_tracks.sql
    Which albums contain more than 20 tracks?

---
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/albums_more_than_20_tracks.sql
@@ -0,0 +1,9 @@
+/*
+Which albums contain more than 20 tracks?
+*/
+SELECT a.AlbumId, a.Title, COUNT(t.TrackId) AS num_tracks
+FROM Album a
+JOIN Track t ON a.AlbumId = t.AlbumId
+GROUP BY a.Title
+HAVING COUNT(t.TrackId)>20
+ORDER BY num_tracks DESC;
