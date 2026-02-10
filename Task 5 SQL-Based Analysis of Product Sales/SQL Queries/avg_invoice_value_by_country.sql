ac07004c3d2c2e91b707b8096423a50fe11cf2c3
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-02-10 19:19:13 +0200

    Create avg_invoice_value_by_country.sql

--- Task 5 SQL-Based Analysis of Product Sales/SQL Queries/avg_invoice_value_by_country.sql
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/avg_invoice_value_by_country.sql
@@ -3,5 +3,5 @@ What is the average invoice total for each billing country?
 */
 SELECT BillingCountry,ROUND(AVG(Total), 2) AS avg_invoice
 FROM Invoice
-GROUP BY BillingCountry
+GROUP BY BillingCountry 
 ORDER BY avg_invoice DESC;
