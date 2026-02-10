116f875c275eb3036049d3e2cdf21217a93c8722
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-02-10 19:18:51 +0200

    Add SQL query to count tracks in each playlist

--- Task 5 SQL-Based Analysis of Product Sales/SQL Queries/playlist_size.sql
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/playlist_size.sql
@@ -3,7 +3,7 @@ How many tracks are in each playlist?
 */
 SELECT 
     p.Name AS playlist,
-    COUNT(pt.TrackId) AS track_count
+    COUNT(pt.TrackId) AS track_count 
 FROM Playlist p
 JOIN PlaylistTrack pt ON p.PlaylistId = pt.PlaylistId
 GROUP BY p.PlaylistId
