6bda00704443bec3211f7a4ece9a74d31e232bae
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2026-02-14 23:54:06 +0200

    Update top_genres_by_revenue.sql

--- Task 5 SQL-Based Analysis of Product Sales/SQL Queries/top_genres_by_revenue.sql
+++ Task 5 SQL-Based Analysis of Product Sales/SQL Queries/top_genres_by_revenue.sql
@@ -1,7 +1,7 @@
 /*
 Which music genres generate the most revenue?
 */
-SELECT g.name, SUM(ROUND(i.UnitPrice * i.Quantity u)) AS Revenue
+SELECT g.name, SUM(ROUND(i.UnitPrice * i.Quantity)) AS Revenue
 FROM Genre g
 JOIN Track t ON g.GenreId = t.GenreId
 JOIN InvoiceLine i ON t.TrackId = i.TrackId
